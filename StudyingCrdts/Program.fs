ef7c2d592054680a79c1f728526642b4ac76c59f
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-11-15 15:51:42 -0300

    chore: remove unnecessary reference files from fsproj

--- StudyingCrdts/Program.fs
+++ StudyingCrdts/Program.fs
@@ -4,5 +4,8 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    let mySequence = seq{ for i in 1..10 do yield i * i }
+    printfn "%A" mySequence
+    StudyingCrdts.PNCounter.function2<Int64> 5L 2L 
     printfn "Hello World from F#!"
     0 // return an integer exit code
